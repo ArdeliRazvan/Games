43af28f5f3f4b38a0a3edb4e4329dbdc422cdede
Ardeli <ardelirazvan9@gmail.com>
2024-11-04 15:00:08 +0200

    Snake-Game Completed

---
+++ GameState.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+using System.Collections.Generic;
+
+namespace Snake_Game
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver {  get; private set; }
+        private readonly Dictionary<Direction, bool> keys = new Dictionary<Direction, bool>();
+        private readonly LinkedList<Position> snakePosition = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+            AddFood();
+        }
+        public void ChangeDirection(Direction dir)
+        {
+            Dir= dir;
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+
+            for(int c = 1; c<= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePosition.AddFirst(new Position(r, c));
+
+            }
+        }
+
+        public IEnumerable<Position> EmptyPositions()
+        {
+            for(int r = 0; r < Rows; r++)
+            {
+                for(int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                        yield return new Position(r, c);
+                }
+            }
+        }
+
+        public void AddFood()
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+            if (empty.Count == 0)
+             return; 
+
+            Position pos = empty[random.Next(empty.Count)];
+           Grid[pos.Row, pos.Col] = GridValue.Food;
+        }
+
+        public Position HeadPoisition()
+        {
+            return snakePosition.First.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return snakePosition.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePositions()
+        {
+            return snakePosition;
+        }
+
+        private void AddHead(Position pos)
+        {
+            snakePosition.AddFirst(pos);
+            Grid[pos.Row, pos.Col] = GridValue.Snake;
+        }
+
+        private void RemoveTail() 
+        {
+            Position tail = snakePosition.Last.Value;
+            Grid[tail.Row, tail.Col] = GridValue.Empty;
+            snakePosition.RemoveLast();
+        }
+        
+
+        private bool OutSideGrid(Position pos)
+        {
+            return pos.Row < 0 || pos.Row >= Rows || pos.Col < 0 || pos.Col >= Cols; 
+        }
+
+        private GridValue WillHit(Position newHeadPos)
+        {
+            if (OutSideGrid(newHeadPos))
+                return GridValue.Outside;
+
+            if (newHeadPos == TailPosition())
+                return GridValue.Empty;
+
+            return Grid[newHeadPos.Row, newHeadPos.Col];    
+        }
+
+        public void Move()
+        {
+            Position newHeadPos = HeadPoisition().Translate(Dir);
+            GridValue hit = WillHit(newHeadPos);
+
+            if (hit == GridValue.Outside || hit == GridValue.Snake)
+                GameOver = true;
+            else if (hit == GridValue.Empty)
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+            }
+            else if (hit == GridValue.Food)
+            {
+                AddHead(newHeadPos);
+                Score++;
+                AddFood();
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Snake_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GameState gameState;
+        public readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages;
+        private bool gameRunning;
+        private readonly Dictionary<GridValue, ImageSource> gridValueImage = new()
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.Body },
+            { GridValue.Food, Images.Food }
+        };
+        private async Task GameLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(100);
+                gameState.Move();
+                Draw();
+            }
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
+        }
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for(int r = 0; r < rows; r++)
+            {
+                for(int c = 0; c < cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+            return images;
+        }
+        private  void Draw()
+        {
+            DrawGrid();
+            ScoreText.Text = $"SCORE {gameState.Score}";
+        }
+
+        private async Task RunGame()
+        {
+           Draw();
+            await ShowCountDown();
+            OverLay.Visibility = Visibility.Hidden;
+             await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver)
+                return;
+            switch (e.Key)
+            {
+                case Key.Left:
+                    if(gameState.Dir != Direction.Right)
+                        gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.Right:
+                    gameState.ChangeDirection(Direction.Right);
+                    break;
+                case Key.Down:
+                    gameState.ChangeDirection(Direction.Down);
+                    break;
+                case Key.Up:
+                    gameState.ChangeDirection(Direction.Up);
+                    break;
+            }
+
+        }
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(OverLay.Visibility == Visibility.Visible)
+                e.Handled = true;
+
+            if(!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
+        }
+
+        private void DrawGrid()
+        {
+            for(int r = 0; r < rows; r++) 
+            {
+                for(int c = 0; c < cols; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValueImage[gridVal];
+                }
+            }
+        }
+        private async Task ShowCountDown()
+        {
+            for(int i = 3; i >= 1; i--)
+            {
+                OverLayText.Text = i.ToString();
+                await Task.Delay(500);
+            }
+        }
+        private async Task ShowGameOver()
+        {
+            await Task.Delay(1000);
+            OverLay.Visibility = Visibility.Visible;
+            OverLayText.Text = "PRESS ANY KEY TO START";
+        }
+    }
+}
